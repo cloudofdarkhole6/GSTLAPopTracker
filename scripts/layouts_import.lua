baabff3fd6e7b65677defa0d74a60f50eaf2bb2c
cloudofdarkhole6 <bernd.schimek@gmail.com>
2026-06-24 17:12:53 +0200

    Add maps for dungeons and towns

--- scripts/layouts_import.lua
+++ scripts/layouts_import.lua
@@ -1,6 +1,7 @@
 Tracker:AddLayouts("layouts/items.jsonc")
 Tracker:AddLayouts("layouts/tracker.jsonc")
 Tracker:AddLayouts("layouts/settings.jsonc")
+Tracker:AddLayouts("layouts/tabs.jsonc")
 
 Tracker:AddLayouts("layouts/broadcast.jsonc")
 -- Tracker:AddLayouts("layouts/dungeon_items_keydrop.json")
